012cb9e623f719bc83f9427ef89fe6071b5baead
Monica <monicaawang.1997@gmail.com>
2021-01-12 21:16:30 -0800

    Create table_creation.sql

---
+++ table_creation.sql
@@ -0,0 +1,15 @@
+CREATE TABLE lifeexpectancy (
+	Location varchar,
+	Period integer,
+	life_expectancy float	
+);
+
+DROP TABLE lifeexpectancy;
+
+CREATE TABLE birthrate (
+	Location varchar,
+	Period integer,
+	birth_rate float	
+);
+
+DROP TABLE birthrate;
